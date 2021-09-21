160117c74809278bf45811578607da970a2d4ebe
piezzacondoleezza <76393917+piezzacondoleezza@users.noreply.github.com>
2021-09-21 17:00:52 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include "orderBook.h"
+#include <_types/_uint32_t.h>
+#include <cstdint>
+#include <exception>
+#include <iostream>
+#include <string>
+
+
+
+
+template<typename T>
+void PrintOptionalPair(std::string streamWord, std::optional<Params<T>> value) {
+    std::cout << streamWord << "price = ";
+    if (value) {
+        std::cout << value->price << ", amount =" << value->amount << std::endl;
+    } else {
+        std::cout << "does not exist" << std::endl;
+    }
+}
+
+template<typename T>
+void PrintOptionalValue(std::string streamWord, std::optional<T> value) {
+    std::cout << streamWord << "price = ";
+    if (value) {
+        std::cout << *value << std::endl;
+    } else {
+        std::cout << "does not exist" << std::endl;
+    }
+}
+
+
+int main(int argc, char** argv) {
+    try {
+        OrderBook<double> orderBook;
+        int id = 1;
+        while (id < argc) {
+            std::string cur = argv[id];
+            if (cur == "GetTopBidAskOfTheBook") {
+                auto [bid, ask] = orderBook.GetTopBidAskOfTheBook();
+                PrintOptionalPair<double>("top bid: ", bid);
+                PrintOptionalPair<double>("lowest ask: ", ask);
+            } else if (cur == "GetTopBidAskOfTheBookWithoutAmount") {
+                auto [bid, ask] = orderBook.GetTopBidAskOfTheBookWithoutAmount();
+                PrintOptionalValue<double>("top bid", bid);
+                PrintOptionalValue<double>("lowest ask", ask);
+            } else if (cur == "UpdateAskPriceAndAmount") {
+                if (id + 2 >= argc) {
+                    throw std::exception();
+                }
+                double price = std::stod(argv[id + 1]);
+                uint32_t amount = std::stoull(argv[id + 2]);
+                id += 2;
+                orderBook.UpdateAskPriceAndAmount(price, amount);
+            } else if (cur == "UpdateBidPriceAndAmount") {
+                if (id + 2 >= argc) {
+                    throw std::exception();
+                }
+                double price = std::stod(argv[id + 1]);
+                uint32_t amount = std::stoull(argv[id + 2]);
+                id += 2;
+                orderBook.UpdateBidPriceAndAmount(price, amount);
+            } else if (cur == "UpdateTheBookFromSnapshot") {
+                //...
+            }
+            ++id;
+        }
+    } catch (...) {
+        std::cerr << "Oops something went wrong" << std::endl;
+    }
+    
+	return 0;
+}
---
+++ orderBook.cpp
@@ -0,0 +1,1 @@
+#include "orderBook.h"
