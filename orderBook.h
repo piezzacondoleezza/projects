4ec36f0025614983df4db89cc1de83cb2f6d8b4a
piezzacondoleezza <76393917+piezzacondoleezza@users.noreply.github.com>
2021-10-02 22:53:37 +0300

    fixed extra map call in update

--- orderBook.h
+++ orderBook.h
@@ -30,20 +30,17 @@ class OrderBook {
 public:
     OrderBook<T>() = default;
 
-    OrderBook<T>(const std::map<T, int>& askMap_,
-        const std::map<T, int>& bidMap_) : 
+    OrderBook<T>(const std::map<T, uint32_t>& askMap_,
+        const std::map<T, uint32_t>& bidMap_) : 
         askMap(askMap_), 
         bidMap(bidMap_) 
         {}
     
-    OrderBook<T>(OrderBook<T>&& other) {
-        askMap = other.orderMap;
-        bidMap = other.bidMap;
+    OrderBook<T>(OrderBook<T>&& other) : askMap(std::forward<std::map<T, uint32_t>>(other.askMap)),
+                bidMap(std::forward<std::map<T, uint32_t>>(other.bidMap)) {
     }
 
-    OrderBook<T>(const OrderBook<T>& other) {
-        askMap = other.askMap;
-        bidMap = other.bidMap;
+    OrderBook<T>(const OrderBook<T>& other) : askMap(other.askMap), bidMap(other.bidMap) {
     }
 
     OrderBook<T>& operator=(const OrderBook<T>& other) {
@@ -53,16 +50,24 @@ public:
     }
 
     void UpdateAskPriceAndAmount(T price, uint32_t amount) {
-        askMap[price] = amount;
         if (amount == 0) {
-            askMap.erase(askMap.find(price));
+            auto iterator = askMap.find(price);
+            if (iterator != askMap.end()) {
+                askMap.erase(iterator);
+            }
+        } else {
+            askMap[price] = amount;
         }
     }
     
     void UpdateBidPriceAndAmount(T price, uint32_t amount) {
-        bidMap[price] = amount;
         if (amount == 0) {
-            bidMap.erase(bidMap.find(price));
+            auto iterator = bidMap.find(price);
+            if (iterator != bidMap.end()) {
+                bidMap.erase(iterator);
+            }
+        } else {
+            bidMap[price] = amount;
         }
     }
 
@@ -89,6 +94,6 @@ public:
     }
 
 private:
-    std::map<T, int> askMap;
-    std::map<T, int> bidMap;
+    std::map<T, uint32_t> askMap;
+    std::map<T, uint32_t> bidMap;
 };
